7f7542594edc42944a20e9ad79a615cea74ed891
ymeddah <my.meddah@gmail.com>
2018-03-28 22:06:02 +0200

    chore(ui): WIP styling report

--- src/template/validations.html.ts
+++ src/template/validations.html.ts
@@ -1,13 +1,14 @@
-import { Validation } from '../model/validation.model';
+import {Validation} from '../model/validation.model';
 
 export const VALIDATIONS = (validations: Validation[]) => {
-  let content: string = '';
-  for (const validation of validations) {
-    content += `<tr><td>${validation.rule}</td><td>${validation.className}</td><td>${validation.error}</td></tr>`;
-  }
-  return `<!DOCTYPE html>
+    let content: string = '';
+    for (const validation of validations) {
+        content += `<tr><td>${validation.rule}</td><td>${validation.className}</td><td>${validation.error}</td></tr>`;
+    }
+    return `<!DOCTYPE html>
         <html>
             <head>
+                <link href="style.css" rel="stylesheet">
                 <style>
                     table {
                       border-spacing: 0;
@@ -33,6 +34,16 @@ export const VALIDATIONS = (validations: Validation[]) => {
                 </style>
             </head>            
           <body>
+            <div class="toolbar">
+                <div class="toolbar-row">
+                    <div class="toolbar-menu">
+                        <ul class="menu-elements">
+                            <li class="menu-element"><a class="menu-link" href="index.html">graph</a></li>
+                            <li class="menu-element"><a class="menu-link" href="validations.html">validations</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
             <h4>Analysis result for validations : ${validations.length} modules identified with errors</h4>
             <table>
                 <tr>
