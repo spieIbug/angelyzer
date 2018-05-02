19d8c64ae22345c255e8c70da6618fd5250ed84f
Maamar Yacine MEDDAH <my.meddah@gmail.com>
2018-05-02 20:04:10 +0200

    fix(CoreModule): error display html fix

--- src/template/element-not-in-wright-place.template.ts
+++ src/template/element-not-in-wright-place.template.ts
@@ -1,8 +1,8 @@
 export const elementNotInWrightPlace = (data: string, prop: string) => (`The element ${data} is not allowed in ${prop}`);
 export const elementsNotInWrightPlace = (datas: string[]) => {
-  let html = '';
-  for (const data of datas) {
-    html += `<p>${data}</p>`;
-  }
-  return html;
+    let html = '';
+    for (const data of datas) {
+        html += `<p>${data}</p>`;
+    }
+    return html;
 };
--- src/template/for-root.template.ts
+++ src/template/for-root.template.ts
@@ -1,17 +1,16 @@
-import { AngularModule } from '../model/angular-module.model';
+import {AngularModule} from '../model/angular-module.model';
 
 export const forRootRecommandationTemplate = (module: AngularModule) => (
-  `<p>The module ${module.name} has exports ${JSON.stringify(module.exports)} and providers ${JSON.stringify(module.providers)}</p>
-   <p>You should have a :</p>
+    `The module ${module.name} has exports <code><pre>${JSON.stringify(module.exports, null, 2)}</pre></code>and providers<code><pre>${JSON.stringify(module.providers, null, 2)}</pre></code>
+   You should have a :
    <code>
       <pre>static forRoot(): ModuleWithProviders {
-        return {
-          ngModule: ${module.name},
-          providers: ${JSON.stringify(module.providers).replace(/"/g, '')}
-        };
-      }</pre>
-    </code>
-    <p>Or define a CoreModule that only provide ${JSON.stringify(module.providers)}</p>
-    <p>Becarefull : the forRoot is for AppModule not children.</p>
+    return {
+      ngModule: ${module.name},
+      providers: ${JSON.stringify(module.providers, null, 8).replace(/"/g, '')}
+    };
+  }</pre>
+    </code>Or define a CoreModule that only provide <br/>${JSON.stringify(module.providers, null, 2)}<br/>
+    Becarefull : the forRoot is for AppModule not children.<br/>
     <a href="https://angular.io/guide/singleton-services">@see</a> & <a href="https://angular.io/guide/ngmodule-faq#why-is-it-bad-if-a-shared-module-provides-a-service-to-a-lazy-loaded-module">@see</a>`
 );
