4b00dccc6ad924b9a338a36faa7a9b724df7c4b3
Maamar Yacine MEDDAH <my.meddah@gmail.com>
2018-05-12 17:48:34 +0200

    chore(logo): added angelyzer logo on scan launch

--- dist/index.js
+++ dist/index.js
@@ -11,7 +11,9 @@ commander.version('1.0.5').description('Angular analyzer');
 commander.command('scan <modulePath> <savePath>').description('Scan an angular project')
     .action(function (modulePath, savePath) {
     var effectiveSavePath = savePath + '/angelyzer_report/';
-    console.log(chalk_1.default.red("=========*** scanning " + modulePath + " ***=========="));
+    console.log(chalk_1.default.red("\n     ___   ___     _   _____    ______   _      __    __  ______   ______   _____   \n    /   | |   \\   | | |  ___|  |  ____| | |     \\ \\  / / |  ____| |  ____| |  __  |\n   / /| | | |\\ \\  | | | |  __  | |__    | |      \\ \\/ /   \\ \\     | |__    | |__| |\n  / /_| | | | \\ \\ | | | | |  | |  __|   | |       \\  /     \\ \\    |  __|   | |__ '\n /  __  | | |  \\ \\| | | |__| | | |____  | |____   / /    ___\\ \\   | |____  | |  \\ \\\n/_/   |_| |_|   \\___| |______| |______| |______| /_/    |______|  |______| |_|   \\_\\\n"));
+    console.log(chalk_1.default.white('Angular project analyzer @author: Yacine MEDDAH <my.meddah@gmail.com>'));
+    console.log(chalk_1.default.yellow("Scanning " + modulePath));
     mkdirp.sync(effectiveSavePath);
     var scanner = new scanner_1.Scanner();
     fs.readdir(modulePath, function (err, files) { return scanner.scanPath(files, modulePath, effectiveSavePath); });
--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,33 @@
 import './polyfills';
 import * as commander from 'commander';
 import chalk from 'chalk';
-import { Scanner } from './scanner';
+import {Scanner} from './scanner';
 import fs = require('fs')
 const mkdirp = require('mkdirp');
 
 commander.version('1.0.5').description('Angular analyzer');
 
+
 commander.command('scan <modulePath> <savePath>').description('Scan an angular project')
-  .action((modulePath, savePath) => {
-    const effectiveSavePath = savePath + '/angelyzer_report/';
-    console.log(chalk.red(`=========*** scanning ${modulePath} ***==========`));
-    mkdirp.sync(effectiveSavePath);
-    const scanner = new Scanner();
-    fs.readdir(modulePath, (err, files) => scanner.scanPath(files, modulePath, effectiveSavePath));
-  });
+    .action((modulePath, savePath) => {
+        const effectiveSavePath = savePath + '/angelyzer_report/';
+        console.log(chalk.red(`
+     ___   ___     _   _____    ______   _      __    __  ______   ______   _____   
+    /   | |   \\   | | |  ___|  |  ____| | |     \\ \\  / / |  ____| |  ____| |  __  |
+   / /| | | |\\ \\  | | | |  __  | |__    | |      \\ \\/ /   \\ \\     | |__    | |__| |
+  / /_| | | | \\ \\ | | | | |  | |  __|   | |       \\  /     \\ \\    |  __|   | |__ '
+ /  __  | | |  \\ \\| | | |__| | | |____  | |____   / /    ___\\ \\   | |____  | |  \\ \\
+/_/   |_| |_|   \\___| |______| |______| |______| /_/    |______|  |______| |_|   \\_\\
+`));
+        console.log(chalk.white('Angular project analyzer @author: Yacine MEDDAH <my.meddah@gmail.com>'))
+        console.log(chalk.yellow(`Scanning ${modulePath}`));
+        mkdirp.sync(effectiveSavePath);
+        const scanner = new Scanner();
+        fs.readdir(modulePath, (err, files) => scanner.scanPath(files, modulePath, effectiveSavePath));
+    });
 
 if (!process.argv.slice(2).length) {
-  commander.outputHelp();
-  process.exit();
+    commander.outputHelp();
+    process.exit();
 }
 commander.parse(process.argv);
